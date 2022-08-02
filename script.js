87295a12a703c547bb47527b1018be20b8478d4c
enceps <spence.d.cole@gmail.com>
2022-08-01 20:54:20 -0400

    change colour based off current hour vs schdeduled hour

--- script.js
+++ script.js
@@ -28,7 +28,29 @@ $("#5PM .description").val(localStorage.getItem("5PM"));
   
 }
 
+    //get current time
+    var currentTime = moment().hour();
+  // parse the hour integer and check against current hour
+  $(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(timeBlock) 
+    if (timeBlock < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (timeBlock === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
 
+    }
+})
 
 timeTracker();
  });
