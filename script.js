9945d77035b3fc87a6c86a4c88ef255f1a3b4a33
enceps <spence.d.cole@gmail.com>
2022-08-01 20:49:57 -0400

    add ability to pull text from local storage for each time block

--- script.js
+++ script.js
@@ -5,13 +5,32 @@ $("#currentDay").html(todayDate);
 $(document).ready(function () {
   // saveBtn click listener 
   $(".saveBtn").on("click", function () {
-      // Get nearby values of the description in JQuery
+      // Get task and time using jQuery
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
 
-      // Save text in local storage
+      // Save local storage
       localStorage.setItem(time, text);
   });
+  function timeTracker() {
+    //Update current schedule
+$("#8AM .description").val(localStorage.getItem("8AM"));
+$("#9AM .description").val(localStorage.getItem("9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11AM .description").val(localStorage.getItem("11AM"));
+$("#12PM .description").val(localStorage.getItem("12PM"));
+$("#1PM .description").val(localStorage.getItem("1PM"));
+$("#2PM .description").val(localStorage.getItem("2PM"));
+$("#3PM .description").val(localStorage.getItem("3PM"));
+$("#4PM .description").val(localStorage.getItem("4PM"));
+$("#5PM .description").val(localStorage.getItem("5PM"));
+
+  
+}
+
+
+
+timeTracker();
  });
 
 
